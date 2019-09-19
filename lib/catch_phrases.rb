d482c1c8101b350102a974e3e4c902e085ea7b75
Faith Janicek <faithe1937@gmail.com>
2019-09-19 01:33:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,12 +9,10 @@ def toadstool
 end
 
 def link
-  saying = "It's Dangerous To Go Alone! Take This."
-  puts saying
+  puts "It's Dangerous To Go Alone! Take This."
 end
   
 def any_phrase(phrase)
   phrase = "Do A Barrel Roll!"
-  puts "#{phrase}"
+  puts phrase
 end
-any_phrase
