0de6884debff5cac61dd60e01944fef86e112b25
Faith Janicek <faithe1937@gmail.com>
2019-09-19 01:24:56 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,10 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!"
+end
+any_phrase
+
+def 
